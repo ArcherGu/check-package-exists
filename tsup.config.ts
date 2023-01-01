3e8417f595307a8f95d62c0661498acadc83a691
ArcherGu <649601376@qq.com>
2023-01-01 12:56:13 +0800

    fix: bundle execa into this pkg
    fuck pure ESM!

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  name: 'check-package-exists',
+  target: 'node14',
+  clean: true,
+  splitting: true,
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  noExternal: ['execa'],
+  dts: true,
+})
